7d273e2b0476007ef3981267fd2ae25ed89302b6
Nobhiko Yuasa <yuasys@gmail.com>
2022-10-12 16:58:26 +0900

    Add Examples

---
+++ examples/index.js
@@ -0,0 +1,11 @@
+const total = 42 + 42;
+console.log(total); // -> 1
+console.log("Hi!")
+const MSG = "私からの特別メッセージですよ〜";
+console.log(MSG);
+let myMSG = MSG;
+console.log("これは、"+MSG);
+
+// タグ付きテンプレート関数：変数を文字列に埋め込む
+console.log(`これぞ${myMSG}`);
+console.log(`これまた、「${MSG}」ですね`);
