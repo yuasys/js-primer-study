c5b31a1c778172e2b9901799e6feb367bd45e9d1
yuasys <yuasys@gmail.com>
2022-10-17 00:19:47 +0900

    2 files modified, 1 added

--- index.js
+++
@@ -1,7 +0,0 @@
-
-console.log("正規表現リテラル");
-const numberRegExp = /\d+/; // 1文字以上の数字にマッチする正規表現
-// `numberRegExp`の正規表現が文字列"123"にマッチするかをテストする
-console.log(numberRegExp.test("aaa")); // => true
-console.log(numberRegExp.test("123")); // => true
-console.log(numberRegExp.test("aaa"));
