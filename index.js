e80e33e95c3a8e30fafe7bf0cef6a86b1aa82377
yuasys <yuasys@gmail.com>
2022-10-16 23:47:48 +0900

    1 file modified

--- index.js
+++ index.js
@@ -31,3 +31,6 @@ console.log(object['123']); // => "value"
 // NG: ドット記法では、数値からはじまる識別子は利用できない
 // object.123
 console.log("正規表現");
+const numberRegExp = /\d+/; // 1文字以上の数字にマッチする正規表現
+// `numberRegExp`の正規表現が文字列"123"にマッチするかをテストする
+console.log(numberRegExp.test("123")); // => true
